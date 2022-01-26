c961ecfbe2795498efd3f6d0cfd3702b95521398
19royy <64555594+19royy@users.noreply.github.com>
2022-01-26 02:30:35 -0800

    First commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import AppLoading from 'expo-app-loading';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Themes } from './assets/Themes';
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    Sydney: require('./assets/Fonts/Sydney-Serial-Regular.ttf'),
+    'Sydney-Bold': require('./assets/Fonts/Sydney-Serial-Bold.ttf'),
+  });
+  if (!fontsLoaded) return <AppLoading />;
+  /* ^Don't mind/edit the code above, it's there to load the font for you! */
+  StatusBar.setBarStyle(Themes.light.statusBar);
+  /* ^Don't mind/edit this one either unless you decide to do the dark theme one, in that case, you will have to change it accordingly*/
+
+  /* insert your code here */
+
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontFamily: 'Sydney', // test to see if the font is loaded, feel free to remove this
+        }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text
+        style={{
+          fontFamily: 'Sydney-Bold', // test to see if the font is loaded, feel free to remove this
+        }}>
+        ~Good luck~
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
